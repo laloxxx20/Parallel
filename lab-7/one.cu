cdb021a7b83c209f507b8ce682ef21b301eeaff4
laloxxx20 <eduardolalox90@gmail.com>
2017-05-26 08:53:47 -0500

    lab 7 cuda

--- lab-7/one.cu
+++ lab-7/one.cu
@@ -30,30 +30,25 @@ void vecAdd(float* A, float* B, float* C, int n)
     B = (float*)malloc(size);
     srand (time(NULL));
 
-    for( int i = 0; i < n; i++ ) 
+    for( int i = 0; i < n; i++ )
     {
-        A[i] = rand() % n + 1;
-        B[i] = rand() % n + 1;
+        A[i] = rand() % n + 1;;
+        B[i] = rand() % n + 1;;
     }
-
-    print_vec(A, size);
-    cout<<"-------------"<<endl;
-    print_vec(B, size);
 
     float *d_A, *d_B, *d_C;
 
     cudaMalloc((void**)&d_A, size);
     cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice);
-
     cudaMalloc((void**)&d_B, size);
     cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
 
-    cudaMalloc((void**)&d_C, size); 
+    cudaMalloc((void**)&d_C, size);
     vecAddKernel<<<ceil((float)n/256.0), 256>>>(d_A, d_B, d_C, n);
     cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
 
-    cudaFree(d_A); 
-    cudaFree(d_B); 
+    cudaFree(d_A);
+    cudaFree(d_B);
     cudaFree(d_C);
 
     print_vec(C, size);
@@ -63,7 +58,7 @@ int main()
 {
     // Size of vectors
     int n = 50;
- 
+
     // Host input vectors
     float* h_a;
     float* h_b;
--- lab-7/two.cu
+++ lab-7/two.cu
@@ -22,18 +22,17 @@ void initMat(float* mat, int n) {
 
 void printVec(float* vector, int size)
 {
-    for(int i=0; i<n; i++)
+    for(int i=0; i<size; i++)
         cout << vector[i] << " ";
     cout<<endl;
 }
 
 void printMat(float *a, int n) {
-    int i, j;
     for(int i=0; i<n; i++){
         for (int j=0; j<n; j++)
             cout<< a[i*n+j] << " ";
     cout<<endl;
-    }    
+    }
 }
 
 __global__
@@ -52,18 +51,24 @@ void mulKernel(float *vec, float *mat, float* c, int n)
 void mulVecMat(float* vec, float* mat, int n){
     float* c;
     float* dev_a, * dev_b, * dev_c;
+    cout<<"is oke"<<endl;
 
     vec = (float*)malloc(sizeof(float)*n);
     mat = (float*)malloc(sizeof(float)*n*n);
     c = (float*)malloc(sizeof(float)*n);
 
-    initArray(vec, n);
-    initMat(mat, n*n);
-    initMat(c, n);
+    cout<<"is oke"<<endl;
 
-    printVec(a, n);
-    printMat(b, n);
-    printVec(c, n);
+    initArray(vec, n);
+    cout<<"array is okey"<<endl;
+    initMat(mat, n*n);
+    cout<<"init ,at is okey"<<endl;
+    initMat(c, n);
+    cout<<"init mat 2 is okey"<<endl;
+
+    // printVec(vec, n);
+    // printMat(mat, n*n);
+    // printVec(c, n);
 
     cudaMalloc((void**)&dev_a, sizeof(float)*n);
     cudaMemcpy(dev_a, vec, sizeof(float)*n, cudaMemcpyHostToDevice);
@@ -71,26 +76,27 @@ void mulVecMat(float* vec, float* mat, int n){
     cudaMalloc((void**)&dev_b, sizeof(float)*n*n);
     cudaMemcpy(dev_b, mat, sizeof(float)*n*n, cudaMemcpyHostToDevice);
 
-    cudaMalloc((void**)&dev_c, sizeof(float)*m);
+    cudaMalloc((void**)&dev_c, sizeof(float)*n);
     mulKernel<<<ceil(n/256.0), 256>>>(dev_a, dev_b, dev_c, n);
     cudaMemcpy(c, dev_c, sizeof(float)*n, cudaMemcpyDeviceToHost);
 
     cudaFree(dev_a);
     cudaFree(dev_b);
     cudaFree(dev_c);
-    
-    printMat(c, n);
+
+    // printMat(c, n);
 }
 
 int main()
 {
     // Size of vectors
     int n = 100;
- 
-    // Host input vectors
-    float* h_a;
-    float* h_ma;
 
-    void mulVecMat(h_a, h_ma, n);
+    // Host input vectors
+    float* h_a = 0;
+    float* h_ma = 0;
+
+    mulVecMat(h_a, h_ma, n);
     return 0;
 }
+
